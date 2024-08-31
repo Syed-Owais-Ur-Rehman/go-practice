59f80a89a10b277269834174779427ff8d7575ff
Syed Owais Ur Rehman <52346980+Syed-Owais-Ur-Rehman@users.noreply.github.com>
2024-08-31 17:49:08 +0500

    arrays and loops

--- types.go
+++ types.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func types() {
 	var name string
-	fmt.Printf("hello %s world %s", name, NewVariable)
+	fmt.Printf("hello '%s' world '%s' \n", name, NewVariable)
 
 	var number int
-	fmt.Printf("hello %b world %s", number, NewVariable)
+	fmt.Printf("hello '%b' world '%s' \n", number, NewVariable)
 }
